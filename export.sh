04e04a66cf8ad38222048842e0e84f1b52974c0a
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2010-06-01 15:58:20 +0000

    update
    git-svn-id: https://sparrow.ece.cmu.edu/svn/edmcman/personal/webpage@456 f64b4bef-b5f7-4cd6-b7eb-c7eeabf07c2e

--- export.sh
+++ export.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 rm -rf ~/afsece/public_html.old
-mv ~/afsece/public_html ~/afsece/public_html.old
-svn export https://sparrow.ece.cmu.edu/svn/edmcman/personal/webpage ~/afsece/public_html/
+svn export https://sparrow.ece.cmu.edu/svn/edmcman/personal/webpage ~/afsece/public_html.old/
+mv ~/afsece/public_html ~/afsece/public_html.old2
+mv ~/afsece/public_html.old ~/afsece/public_html
+mv ~/afsece/public_html.old2 ~/afsece/public_html.old
