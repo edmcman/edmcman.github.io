50d080819998b97d0b046bf22032591bd695324f
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2014-05-16 14:49:45 -0400

    Fix export script

--- export.sh
+++ export.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 rm -rf ~/afsece/public_html.old
-svn export https://sparrow.ece.cmu.edu/svn/edmcman/personal/webpage ~/afsece/public_html.old/
-mv ~/afsece/public_html ~/afsece/public_html.old2
+mkdir ~/afsece/public_html.old
+git archive master | tar -xv -C ~/afsece/public_html.old
+mv ~/afsece/public_html ~/afsece/public_html.old2 || true
 mv ~/afsece/public_html.old ~/afsece/public_html
-mv ~/afsece/public_html.old2 ~/afsece/public_html.old
+mv ~/afsece/public_html.old2 ~/afsece/public_html.old || true
