eadcf437bac3db66f94b0d6312627bfe6aa84230
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2014-11-07 15:39:57 -0500

    Speed up script dramatically

--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -rf ~/afsece/public_html.old
-mkdir ~/afsece/public_html.old
-git archive master | tar -xv -C ~/afsece/public_html.old
+mkdir -p /tmp/webpage
+git archive master | tar -xv -C /tmp/webpage
+rsync -c --progress -avz /tmp/webpage/ ejschwar@grey.ece.cmu.edu:public_html.old
 mv ~/afsece/public_html ~/afsece/public_html.old2 || true
 mv ~/afsece/public_html.old ~/afsece/public_html
 mv ~/afsece/public_html.old2 ~/afsece/public_html.old || true
