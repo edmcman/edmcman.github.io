a1db6a7f0b4f1fd9288244f486f3cb519c74990b
Edward J. Schwartz <eschwartz@cert.org>
2018-08-20 09:34:31 -0400

    Don't need this anymore

--- export.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Note: Log in to grey and run kinit before running script.
-
-mkdir -p /tmp/webpage
-git archive master | tar -xv -C /tmp/webpage
-rsync -c --progress -avz /tmp/webpage/ ejschwar@grey.ece.cmu.edu:public_html.old
-mv ~/afsece/public_html ~/afsece/public_html.old2 || true
-mv ~/afsece/public_html.old ~/afsece/public_html
-mv ~/afsece/public_html.old2 ~/afsece/public_html.old || true
