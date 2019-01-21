7859b74f41e6af6ca042bb549ee3eb12fbcd2595
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2019-01-21 17:51:56 -0500

    Add new blog

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b7fcf78aa7aff78bc1ad.js"
+    "url": "webpack-runtime-7d06181701d6515ce14f.js"
   },
   {
     "url": "app-4ab42c870bbe3530cca9.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0c2c990d5efd232a7ad590ce325161ff"
+    "revision": "4b92761b8fe90d5fc6883d0c2e83d096"
   },
   {
     "url": "0.ab37045fe91d631d21bd.css"
