72fec4378b63e0cc038af718cfb28ddada88a44c
Edward J. Schwartz <eschwartz@cert.org>
2023-11-26 10:42:00 -0500

    blog updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-38c697c3400fde402577.js"
+    "url": "webpack-runtime-8941fdba8da491779752.js"
   },
   {
     "url": "framework-e7350785641ee58ddf86.js"
   },
   {
-    "url": "app-a4ec8a254c6cb8c2cfbc.js"
+    "url": "app-c8e2776639497f1dbbe5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fe5163c92e3a8c684ffa9d05dd253b8e"
+    "revision": "e12ae190241653a6398f9d91d5707aab"
   },
   {
     "url": "manifest.webmanifest",
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-a4ec8a254c6cb8c2cfbc.js`))) {
+  if (!resources || !(await caches.match(`/app-c8e2776639497f1dbbe5.js`))) {
     return await fetch(event.request)
   }
 
